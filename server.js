4add50d8378d5222d0b42f80dcc7dba1641d9680
Gopallature <gopallature@gmail.com>
2020-09-12 16:27:25 +0000

    first commit

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+console.log("gopal");
+
+app.get('/getfunc', (req, res, next) => {
+	res.json({"message": "hello gopal"});
+})
+
 app.listen(3000);
