42f3992205bc8b5ee301399de45b60591b61767f
KIRTISHD <aniket.dhande@gmail.com>
2020-09-13 10:45:47 +0530

    Orders and Products v1

---
+++ controllers/orderController.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
+const Order = require('../models/order');
+
+exports.addOrder = async (req, res, next) => {
+    const amount = req.body.amount;
+    const userid = req.body.userid;
+    const productsOrdered = req.body.products;
+    try{
+
+        const listofProducts = await Product.find().where('_id').in(productsOrdered).exec();
+
+        // listofProducts.forEach(obj => {
+        //     obj.Quantity = obj.Quantity - 1;
+        //     await obj.save();
+        // });
+
+        for (const obj of listofProducts){
+            obj.Quantity = obj.Quantity - 1;
+            await obj.save();
+        }
+        
+        const order = new Order({
+            Amount: amount,
+            Products: productsOrdered,
+            UserID: userid
+        })
+
+        const orderPlaced = await order.save();
+
+        res.status(200).json({message: 'Order Added'});
+    }
+    catch(err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+         next(err);
+    }
+}
+
+exports.getOrders = async (req, res, next) => {
+    const userid = req.params.userid;
+
+    try{
+        const orders = Order.find({'UserID': userid});
+
+        res.status(200).json({message: 'Orders list', orders: orders});
+    }
+    catch(err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+         next(err);
+    }
+}
--- server.js
+++ server.js
@@ -1,5 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
+require('dotenv').config();
 
-app.listen(3000);
+app.use(bodyParser.json());
+
+app.use(cors());
+app.use(helmet());
+
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
+});
+
+app.use('/product', productRoutes);
+app.use('/order', orderRoutes);
+
+mongoose
+    .connect(
+        //'mongodb://' + process.env.MONGO_HOST + ':' + process.env.MONGO_PASSWD + '@cluster0-shard-00-00-vccpc.mongodb.net:27017,cluster0-shard-00-01-vccpc.mongodb.net:27017,cluster0-shard-00-02-vccpc.mongodb.net:27017/' + process.env.DB_NAME + '?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }
+        'mongodb+srv://' + process.env.MONGO_HOST + ':' + process.env.MONGO_PASSWD + '@ecommerce.n5ajl.mongodb.net/' + process.env.DB_NAME + '?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(result => {
+        console.log("Backend Started");
+        app.listen(process.env.PORT || 3000);
+    })
+    .catch(err => console.log("err->" + err));
